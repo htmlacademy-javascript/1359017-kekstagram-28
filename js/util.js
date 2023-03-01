618258cf1b997365bdce104d813fbba516e4171f
Ekaterina <ezakhvataeva@gmail.com>
2023-03-01 15:31:57 +0100

    Выполняет 5 задание, разбивает на модули

---
+++ js/util.js
@@ -0,0 +1,28 @@
+//рандомное значение
+const getRandom = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//добавляем метод для генерации массива для возврата случайного элемента
+const getRandomArrayElement = (elements) => elements [getRandom(0, elements.length - 1)];
+
+// получаем случайное неповторяющееся число.
+const getId = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandom(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandom(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+export {getRandom, getRandomArrayElement, getId};
