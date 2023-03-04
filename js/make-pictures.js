63d477d76a4024e2a99fe4ddedef979625c81c75
Ekaterina <ezakhvataeva@gmail.com>
2023-03-04 11:20:15 +0100

    Выполняет 7 задание/ отрисуй меня полностью

---
+++ js/make-pictures.js
@@ -0,0 +1,20 @@
+import { createMockPhotos } from './data.js';
+
+const picture = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const similarPictures = createMockPhotos(25);
+
+const similarPicturesFragment = document.createDocumentFragment();
+
+
+similarPictures.forEach (({url, comments, likes})=> {
+  const pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  similarPicturesFragment.appendChild(pictureElement);
+});
+
+picture.appendChild(similarPicturesFragment);
+
