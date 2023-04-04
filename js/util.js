0822699d4080d7a0cb04cec32c2d25fc522409ef
ekaterina-bkv <98402123+ekaterina-bkv@users.noreply.github.com>
2023-04-04 10:42:40 +0200

    Revert "Удалила временные данные, предварительная подготовка к защите"

--- js/util.js
+++ js/util.js
@@ -1,3 +1,29 @@
+//рандомное значение
+const getRandom = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//добавляем метод для генерации массива для возврата случайного элемента
+const getRandomArrayElement = (elements) => elements [getRandom(0, elements.length - 1)];
+
+// получаем случайное неповторяющееся число.
+const getId = (min, max) => {
+  const previousValues = [];
+  return function () {
+    let currentValue = getRandom(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandom(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
 const isEscapeKey = (evt) => evt.key === 'Escape';
 const ALERT_SHOW_TIME = 8000;
 
@@ -32,4 +58,4 @@ const debounce = (callback, timeoutDelay) => {
   };
 };
 
-export { isEscapeKey,showAlert, shuffleArray, debounce};
+export {getRandom, getRandomArrayElement, getId, isEscapeKey,showAlert, shuffleArray, debounce};
